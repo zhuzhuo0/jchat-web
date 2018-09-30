0c904f3c9766a4c9f8ae87b627871862dc0caddb
DESKTOP-N0FMIGE\猪猪 <422914219@qq.com>
2018-09-30 09:58:05 +0800

    临时提交

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import '../assets/js/jmessage-sdk-web.2.6.0.min.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+declare var JMessage: any;
+
 export class AppComponent {
   title = 'app';
+
+  jmessage: any;
+
+  constructor() {
+    this.jmessage = new JMessage();
+  }
+
+  imInit() {
+
+  }
 }
